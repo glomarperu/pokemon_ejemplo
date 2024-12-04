179de93de920135494fbbc0776f5adfb3bd525c8
glomarperu <glomarperu1@gmail.com>
2024-12-04 11:38:26 -0500

    nuevos datos

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { PokemonPage } from './components/PokemonPage'
 //import { TypesComponent } from './components/TypesComponent'
 
 function App() {
- 
+  // nuevos datos  1000
   return (
     <>
       { /*<TypesComponent /> */}
